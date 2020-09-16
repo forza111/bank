d0aa23d07dff1bea94ae65cb36ed53747e54c807
forza111 <nikitos.ionkin@yandex.ru>
2020-09-16 09:20:44 +0300

    16.09.2020

--- payment/admin.py
+++ payment/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import User,Balance,Credit
 
 admin.site.register(User)
+admin.site.register(Balance)
+admin.site.register(Credit)
 
 # Register your models here.
