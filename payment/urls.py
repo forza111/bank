b33cde81dfe490ca0dc5e59cb2ff056a05382626
forza111 <nikitos.ionkin@yandex.ru>
2020-09-18 01:27:13 +0300

    create view.detail and detail.html

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'payment'
 urlpatterns=[
     path('', views.IndexView.as_view(),name = 'index'),
-    path('<int:pk>', views.DetailView.as_view(),name = 'detail'),
+    path('<int:user_id>/', views.detail,name = 'detail'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponseRedirect,HttpResponse
+from django.http import HttpResponseRedirect,HttpResponse,Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -18,8 +18,28 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return User.objects.all()
 
-class DetailView(generic.DetailView):
-    model = Balance
-    template_name = 'payment/detail.html'
+def detail(requset, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    check_credit = Credit.objects.filter(user = user).exists()
+    credit = Credit.objects.filter(user = user)
+    try:
+        balance = Balance.objects.get(user = user)
+    except(KeyError,Balance.DoesNotExist):
+        return render(requset, 'payment/detail.html',
+                      {
+                          'user' : user,
+                          'error_message': '{} not balance'.format(user),
+                      'credit':credit,
+                      'check_credit':check_credit})
+    else:
+        return render(requset, 'payment/detail.html',
+                      {'user': user, 'balance' : balance,'credit':credit,
+                      'check_credit':check_credit} )
 
 
+'''def detail(reqest, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        raise Http404('No NO Noo')
+    return render(reqest, 'payment/detail.html', {'user' : user})'''
