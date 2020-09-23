cd7b1539761b490f93d20f35ac1fdca38d8279b4
forza111 <nikitos.ionkin@yandex.ru>
2020-09-23 09:24:03 +0300

    Создание  view функции погашения кредита и templates фаил к нему - раздумья как сделать лучше. Параллельно добавил немного эстетики в html.

--- payment/views.py
+++ payment/views.py
@@ -36,7 +36,9 @@ def detail(requset, user_id):
                       'check_credit':check_credit} )
 def credit_repayment(requset, user_id):
     user = get_object_or_404(User, pk=user_id)
+
     check_credit = Credit.objects.filter(user = user).exists()
     return render(requset, 'payment/credit_repayment.html',
                   {'user': user,
-                   'check_credit': check_credit})
+                   'check_credit': check_credit,
+                   })
