58a7c9ad72aa46cd8e7b169772805799b573e163
forza111 <nikitos.ionkin@yandex.ru>
2020-09-25 15:11:22 +0300

    Создание формы входа и выхода пользователя

--- bank/settings.py
+++ bank/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'bank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL='/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- bank/urls.py
+++ bank/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('payment/',include('payment.urls')),
     path('', include('payment.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
